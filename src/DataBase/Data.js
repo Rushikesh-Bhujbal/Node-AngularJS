079ba710e68ccc3b4a8adcb6c5d0f1ffd9ed42de
Rushikesh-Bhujbal <rushikeshbhujbal92@gmail.com>
2016-06-25 22:11:36 +0530

    Designed Setup

---
+++ src/DataBase/Data.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    Students : [{
+        studentName: "Raj",
+        contact: "1234567890",
+        address: "Pune"
+    },
+        {
+            studentName: "Ram",
+            contact: "9862365895",
+            address: "Nasik"
+        }, {
+            studentName: "Rahul",
+            contact: "1265495365",
+            address: "Nagpur"
+        }, {
+            studentName: "Ramesh",
+            contact: "3695236566",
+            address: "Pune"
+        }]
+}
---
+++ src/client/app/naApp.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('APP_MODULE_NAME', [
+            
+        ]);
+
+}());
